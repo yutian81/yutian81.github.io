d65f8f86f510542d159041784530860cf588e142
yutian81 <165384851+yutian81@users.noreply.github.com>
2024-11-18 09:47:02 +0800

    内置图片压缩工具

---
+++ .vitepress/theme/components/imgc.vue
@@ -0,0 +1,64 @@
+<template>
+  <div id="app" class="common body-pc">
+    <el-upload
+      class="upload-demo"
+      drag
+      multiple
+      :before-upload="() => false"
+      :on-change="handleFileChange"
+    >
+      <i class="el-icon-upload"></i>
+      <div class="el-upload__text">
+        将文件拖到此处，或<em>点击上传</em>
+      </div>
+      <div class="el-upload__tip" slot="tip">
+        支持多文件上传，默认压缩率为60%，可拖动修改！
+      </div>
+    </el-upload>
+
+    <div class="slider-container">
+      <input
+        type="range"
+        id="compressionRatio"
+        min="0"
+        max="100"
+        v-model="compressionRatio"
+        @input="updateCompressionRatio"
+      />
+      <span>{{ compressionRatio }}%</span>
+    </div>
+
+    <div id="preview"></div>
+    <el-button
+      type="success"
+      icon="el-icon-download"
+      @click="downloadCompressedImages"
+    >
+      下载压缩图片
+    </el-button>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      compressionRatio: 60,
+    };
+  },
+  methods: {
+    handleFileChange(file) {
+      console.log("File changed:", file);
+      // 添加文件处理逻辑
+    },
+    updateCompressionRatio() {
+      console.log("Compression ratio updated:", this.compressionRatio);
+      // 更新压缩比逻辑
+    },
+    downloadCompressedImages() {
+      console.log("Downloading compressed images");
+      // 添加下载逻辑
+    },
+  },
+};
+</script>
